b68b3b774e4dfcae84128213a0f3bf11843afc9e
Nikhil Swaminathan <nsswamin@amazon.com>
2018-10-05 13:26:59 -0700

    Added auth

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { withAuthenticator } from 'aws-amplify-react'
 
 class App extends Component {
   render() {
@@ -25,4 +26,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default withAuthenticator(App, true);
